dcd90982a5e93639622629763aeafa875f1e5447
Sheldon Chi <sheldonchi@gmail.com>
2016-05-22 23:27:04 +0800

    Modify valid_number? method

--- programming_and_back-end_development/101_programming_foundations/lesson_2/calculator.rb
+++ programming_and_back-end_development/101_programming_foundations/lesson_2/calculator.rb
@@ -3,7 +3,7 @@ def prompt(message)
 end
 
 def valid_number?(num)
-	num.to_i != 0
+	/^\d+\.?\d+$/.match(num)
 end
 
 def operation_to_message(op)
@@ -74,13 +74,13 @@ loop do
 
 	result = case operator
 					 when '1'
-					 	number1.to_i() + number2.to_i()
+					 	 number1.to_i() + number2.to_i()
 					 when '2'
-					 	number1.to_i() - number2.to_i()
+					 	 number1.to_i() - number2.to_i()
 					 when '3'
-					 	number1.to_i() * number2.to_i()
+					 	 number1.to_i() * number2.to_i()
 					 when '4'
-					 	number1.to_f() / number2.to_f()
+					 	 number1.to_f() / number2.to_f()
 	end
 
 	prompt("The result is #{result}")
