d6566a53539ae2db0f4b02f09d5e681a67d2664f
Sheldon Chi <sheldonchi@gmail.com>
2017-01-31 14:57:59 +0800

    LS Sinatra Todos

---
+++ programming_and_back-end_development/170/book_viewer/book_viewer.rb
@@ -0,0 +1,58 @@
+require "sinatra"
+require "sinatra/reloader"
+require "tilt/erubis"
+
+before do
+  @contents = File.readlines("data/toc.txt")
+end
+
+not_found do
+  redirect "/"
+end
+
+helpers do
+  def in_paragraphs(text)
+    text.split("\n\n").map { |paragraph| "<p>#{paragraph}</p>" }.join
+  end
+end
+
+get "/" do
+  @title = "The Adventures of Sherlock Holmes"
+
+  erb :home
+end
+
+get "/chapters/:number" do
+  number = params[:number].to_i
+  chapter_name = @contents[number - 1]
+  @title = "Chapter #{number}: #{chapter_name}"
+
+  @chapter = File.read("data/chp#{number}.txt")
+
+  erb :chapter
+end
+
+def each_chapter(&block)
+  @contents.each_with_index do |name, index|
+    number = index + 1
+    contents = File.read("data/chp#{number}.txt")
+    yield number, name, contents
+  end
+end
+
+def chapters_matching(query)
+  results = []
+
+  return results unless query
+
+  each_chapter do |number, name, contents|
+    results << {number: number, name: name} if contents.include?(query)
+  end
+
+  results
+end
+
+get "/search" do
+  @results = chapters_matching(params[:query])
+  erb :search
+end
